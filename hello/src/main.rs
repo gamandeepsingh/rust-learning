f3f6312825fa64106b635806fa1f4ac3b333aefc
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-12-16 17:07:18 +0530

    Create: ib function

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,36 @@
 // 1. Create a function that takes an integer as an argument and returns true if the integer is even, false if it is not.
-fn main() {
-    println!("{}",is_even(234567890));
-}
+// fn main() {
+//     println!("{}",is_even(234567890));
+// }
 
-fn is_even(i:i32) -> bool{
-    if i%2==0{
-        return true;
-    }
-    return false;
-}
+// fn is_even(i:i32) -> bool{
+//     if i%2==0{
+//         return true;
+//     }
+//     return false;
+// }
+
+
+
+//  A `fib` function that calculates the Fibonacci number for a given integer using recursion.
+// fn main(){
+//     let a:i32=10;
+//     let b:i32=20;
+//     println!("{} + {} = {}",a,b,a+b);
+//     for i in 0..10 {    
+//         println!("{}",fib(i));
+//     }
+// }
+
+// fn fib(num:i32)->i32{
+//     if num<=1 {
+//         return num;
+//     }
+//     else{
+//         return fib(num-1)+fib(num-2);
+//     }
+// }
+
+
+// A function that takes a string as an argument and returns a length of the string.
+fn main(){}
