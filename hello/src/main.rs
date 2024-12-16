5c7b1cf82fe720fbecd6b9cedd1bf2e3c25a4dd4
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-12-16 19:00:51 +0530

    Implement enums

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -11,14 +11,13 @@
 //     return false;
 // }
 
-
 // --------------------------------------------------------------------------------------------- //
 //  A `fib` function that calculates the Fibonacci number for a given integer using recursion.
 // fn main(){
 //     let a:i32=10;
 //     let b:i32=20;
 //     println!("{} + {} = {}",a,b,a+b);
-//     for i in 0..10 {    
+//     for i in 0..10 {
 //         println!("{}",fib(i));
 //     }
 // }
@@ -44,61 +43,102 @@
 //     s.chars().count()
 // }
 
-
 // --------------------------------------------------------------------------------------------- //
 // Implementaion of Structure using `struct` keyword in Rust with methods using `impl` keyword
-struct User{
-    name:String,
-    email:String,
-    mobile_number:i32,
-    sign_in_count:u64,
-    active:bool
-}
+// struct User {
+//     name: String,
+//     email: String,
+//     mobile_number: i32,
+//     sign_in_count: u64,
+//     active: bool,
+// }
 
 // here `self` is the instance of the structure and `&self` is the reference of the instance like `this` in JS
-impl User {
-    fn display_all(&self){
-        println!("Username: {}",self.name);
-        println!("Email: {}",self.email);
-        println!("Mobile Number: {}",self.mobile_number);
-        println!("SignIn Count: {}",self.sign_in_count);
-        println!("Active: {}",self.active);
-    }
+// impl User {
+//     fn display_all(&self){
+//         println!("Username: {}",self.name);
+//         println!("Email: {}",self.email);
+//         println!("Mobile Number: {}",self.mobile_number);
+//         println!("SignIn Count: {}",self.sign_in_count);
+//         println!("Active: {}",self.active);
+//     }
 
-    fn email_name(&self,i:i32){
-        println!("{}{}{}",self.email,i,self.name.to_lowercase().replace(" ", ""));
-    }
+//     fn email_name(&self,i:i32){
+//         println!("{}{}{}",self.email,i,self.name.to_lowercase().replace(" ", ""));
+//     }
 
-    fn update_email(&mut self,email:String){
-        self.email=email;
-    }
+//     fn update_email(&mut self,email:String){
+//         self.email=email;
+//     }
 
-    // static method : we can call this method without creating an instance of the structure i.e. User::debug()
-    fn debug(){
-        println!("This is a static method");
-    }
-}
+//     // static method : we can call this method without creating an instance of the structure i.e. User::debug()
+//     fn debug(){
+//         println!("This is a static method");
+//     }
+// }
 
-fn main(){
-    let user1:User = User{
-        active:true,
-        email:String::from("gamandeep@gmail.com"),
-        mobile_number:1234567890,
-        name:String::from("Gamandeep Singh"),
-        sign_in_count:60
-    };
-    user1.display_all();
-    user1.email_name(10);
-    let mut user2:User = User{
-        active:true,
-        email:String::from("gamandeep"),
-        mobile_number:1234567890,
-        name:String::from("Gamandeep Singh"),
-        sign_in_count:60
-    };
-    user2.update_email(String::from("gaman@gmail.com"));
-    User::debug()  // calling static method
-}
-
+// fn main(){
+//     let user1:User = User{
+//         active:true,
+//         email:String::from("gamandeep@gmail.com"),
+//         mobile_number:1234567890,
+//         name:String::from("Gamandeep Singh"),
+//         sign_in_count:60
+//     };
+//     user1.display_all();
+//     user1.email_name(10);
+//     let mut user2:User = User{
+//         active:true,
+//         email:String::from("gamandeep"),
+//         mobile_number:1234567890,
+//         name:String::from("Gamandeep Singh"),
+//         sign_in_count:60
+//     };
+//     user2.update_email(String::from("gaman@gmail.com"));
+//     User::debug()  // calling static method
+// }
 
 // --------------------------------------------------------------------------------------------- //
+
+// Implenting enum in Rust
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+enum Shape {
+    Rectangle(f64, f64), // width,height
+    Circle(f64),         // radius
+}
+
+fn main() {
+    let my_direction = Direction::North;
+    move_around(Direction::North);
+    // or
+    move_around(my_direction);
+
+    let rect = Shape::Rectangle(6.0, 5.0);
+    calculate_area(rect);
+    let circle = Shape::Circle(5.0);
+    calculate_area(circle);
+}
+
+fn move_around(arg_direction: Direction) {
+    match arg_direction {
+        Direction::East => println!("East"),
+        Direction::North => println!("North"),
+        Direction::South => println!("South"),
+        Direction::West => println!("West"),
+    }
+    println!("Hi there, inside function");
+}
+
+fn calculate_area(shape: Shape) {
+    let area = match shape {
+        Shape::Circle(r) => 3.14 * r * r,
+        Shape::Rectangle(a, b) => a * b,
+    };
+    print!("{}", area);
+}
